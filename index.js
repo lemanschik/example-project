0b51e6361edc4d7a14428088196195cdd6ace685
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-04-11 11:28:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const assign = (tagName,props) => Object.assign(document.createElement(tagName),props);
-const assignStyle = (el, styleDecl) => Object.assign(el.style,styleDecl);
-const createEl = (tagName,props,styleDecl) => assignStyle(assign(tagName,props),styleDecl);
+const style = (el, styleDecl) => Object.assign(el.style,styleDecl);
+const createEl = (tagName,{style,...props}) => !style ? assign(tagName,props) : assignStyle(assign(tagName,props),style);
 
 // This Appends DOMNodes
 document.body.appendChild;
@@ -8,18 +8,24 @@ document.body.appendChild;
 document.body.append;
 // A DOMNode's .outerHTML represents the String Note metthods like onclick do not get serialized from alone. 
 
-const h1 = (innerText)=>assign('h1',{innerText});
-const h2 = (innerText)=>assign('h2',{innerText});
-const p = (innerText)=>assign('p',{innerText})
+const h1 = (innerText,style)=>createEl('h1',{innerText,style});
+const h2 = (innerText,style)=>createEl('h2',{innerText,style});
+const p = (innerText,style)=>createEl('p',{innerText,style});
+const img = (src,style)=>createEl('img',{src,style});
 
 [h1('Welcome to the Homepage'),// vs assign('h1',{ innerText: 'Welcome to the Homepage' }),
 p('This is logical for me.'),
-h2('Header 2.'),
+style(h2('Header 2.'), { color: 'red' }),
 p('This is logical for me.'),
+img('data:base64,....',{ width: '20px', height: '30px' }),
+createEl({ src: 'data:base64,....',style:{ width: '20px', height: '30px' }})
 ]
 // Render
 .map(node=>document.body.appendChild(node) || node)
 // SSR
 .forEach(node=>console.log(node.outerHTML);
-         
-document.body.append(`<h1>Welcome to the Homepage</h1><p>This is logical for me.</p><h1>Header 2</h1><p>This is logical for me.</p>`);
+
+//will not work like innerHTML because the string gets auto escaped.       
+//document.body.append(`<h1>Welcome to the Homepage</h1><p>This is logical for me.</p><h1>Header 2</h1><p>This is logical for me.</p>`);
+
+
